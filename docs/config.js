002722aaa1e2bb7fa9bf1bfa885d322d3a6a9cf5
Niklaus8030 <leadinggrand@gmail.com>
2024-02-26 21:00:31 +0300

    url

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,6 @@
 export default {
   OFFlimits_logo:
-    "https://cdn.discordapp.com/attachments/851632709527666708/868999341979869224/OFFlimits.png",
+    "https://firebasestorage.googleapis.com/v0/b/offlimits-bot.appspot.com/o/boards%2Fother%2FOFFlimits.png?alt=media",
   botInvite:
     "https://discord.com/oauth2/authorize?client_id=728332591790293044&scope=bot+applications.commands&permissions=268445752&client_id=728332591790293044",
 };
